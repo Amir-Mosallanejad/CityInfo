22a29d9ab211935be7ea33a98262f264b349e15c
Amir Mosallanejad <mosallanejad.amir@gmail.com>
2025-08-26 13:17:13 +0330

    Add Cities, Files and PointsOfInterest endpoints
    -add xml request/response support added in program.cs
    
    - you can get file format with FileExtensionContentTypeProvider that registered in Program.cs and used in FilesController.cs

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,13 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.StaticFiles;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => { options.ReturnHttpNotAcceptable = true; })
+    .AddXmlDataContractSerializerFormatters();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -18,8 +23,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.Run();
