38dc85cebf4b05c4567d69e571f9530cc2f32261
Amir Mosallanejad <mosallanejad.amir@gmail.com>
2025-08-27 10:34:07 +0330

    Implement DataBase Structure with SqlLite

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,38 +1,37 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CityInfo.API.Controllers
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
 {
-    [ApiController]
-    [Route("api/cities")]
-    public class CitiesController : ControllerBase
+    private readonly CitiesDataStore _citiesDataStore;
+
+    public CitiesController(CitiesDataStore citiesDataStore)
     {
-        private readonly CitiesDataStore _citiesDataStore;
+        _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+    }
 
-        public CitiesController(CitiesDataStore citiesDataStore)
+    [HttpGet]
+    public ActionResult<IEnumerable<CityDto>> GetCities()
+    { 
+        return Ok(_citiesDataStore.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<CityDto> GetCity(int id)
+    {
+        // find city
+        CityDto? cityToReturn = _citiesDataStore.Cities
+            .FirstOrDefault(c => c.Id == id);
+
+        if (cityToReturn == null)
         {
-            _citiesDataStore = citiesDataStore ?? throw new ArgumentNullException(nameof(citiesDataStore));
+            return NotFound();
         }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<CityDto>> GetCities()
-        { 
-            return Ok(_citiesDataStore.Cities);
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<CityDto> GetCity(int id)
-        {
-            // find city
-            CityDto? cityToReturn = _citiesDataStore.Cities
-                .FirstOrDefault(c => c.Id == id);
-
-            if (cityToReturn == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(cityToReturn);
-        }
+        return Ok(cityToReturn);
     }
 }
